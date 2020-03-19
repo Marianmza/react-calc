4f3b83f2a0270792c8e7bd84d3b36acf2a499c39
Slavo Kozar <slavo.kozar@gmail.com>
2020-03-20 00:38:13 +0100

    template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="calc">
+            <div className="display">
+                <input type="number" value={123} disabled/>
+            </div>
+            <div className="buttons">
+                <div className="left">
+                    <button>0</button>
+                    <button className="clear">C</button>
+                    <button className="equal">=</button>
+                    {
+                        ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {
+                            const number = i + 1;
+
+                            return (
+                                <button>{number}</button>
+                            )
+                        } )
+                    }
+                </div>
+                <div className="actions">
+                    <button>&divide;</button>
+                    <button>&times;</button>
+                    <button>-</button>
+                    <button>+</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
