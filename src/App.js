fca71ed55d9db5a6a5fa0b75636cf49763bb5227
Slavo Kozar <slavo.kozar@gmail.com>
2020-03-20 09:55:49 +0100

    entering first number

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="calc">
-            <div className="display">
-                <input type="number" value={123} disabled/>
-            </div>
-            <div className="buttons">
-                <div className="left">
-                    <button>0</button>
-                    <button className="clear">C</button>
-                    <button className="equal">=</button>
-                    {
-                        ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {
-                            const number = i + 1;
+class App extends Component {
+    constructor(){
+        super();
 
-                            return (
-                                <button>{number}</button>
-                            )
-                        } )
-                    }
+        this.state = {
+            number1: 0,
+            number2: 0
+        }
+    }
+
+    render() {
+        return (
+            <div className="calc">
+                <div className="display">
+                    <input type="number" value={this.state.number1} disabled/>
                 </div>
-                <div className="actions">
-                    <button>&divide;</button>
-                    <button>&times;</button>
-                    <button>-</button>
-                    <button>+</button>
+                <div className="buttons">
+                    <div className="left">
+                        <button>0</button>
+                        <button className="clear">C</button>
+                        <button className="equal">=</button>
+                        {
+                            ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {
+                                const number = i + 1;
+
+                                return (
+                                    <button
+                                        onClick={() => {
+                                            this.setState({number1: this.state.number1 * 10 + number})
+                                        }}
+                                    >
+                                        {number}
+                                    </button>
+                                )
+                            } )
+                        }
+                    </div>
+                    <div className="actions">
+                        <button>&divide;</button>
+                        <button>&times;</button>
+                        <button>-</button>
+                        <button>+</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
