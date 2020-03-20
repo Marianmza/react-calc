a97b49db738f661f9b496fd53c64b60ebe8d1a8f
Slavo Kozar <slavo.kozar@gmail.com>
2020-03-20 10:23:06 +0100

    second number

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
             number1: 0,
             number2: 0,
             operand: null,
-            number: 1,
+            number:  1,
         }
     }
 
@@ -17,13 +17,28 @@ class App extends Component {
         return (
             <div className="calc">
                 <div className="display">
-                    <input type="number" value={this.state.number === 1 ? this.state.number1 : this.state.number2} disabled/>
+                    <input type="number" value={this.state.number === 1 ? this.state.number1 : this.state.number2}
+                           disabled/>
                 </div>
                 <div className="buttons">
                     <div className="left">
                         <button>0</button>
-                        <button className="clear">C</button>
-                        <button className="equal">=</button>
+                        <button
+                            className="clear"
+                            onClick={() => {
+
+                            }}
+                        >
+                            C
+                        </button>
+                        <button
+                            className="equal"
+                            onClick={() => {
+
+                            }}
+                        >
+                            =
+                        </button>
                         {
                             ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {
                                 const number = i + 1;
@@ -31,10 +46,10 @@ class App extends Component {
                                 return (
                                     <button
                                         onClick={() => {
-                                            if(this.state.number == 1){
-                                                this.setState({number1: this.state.number1 * 10 + number})
-                                            }else{
-                                                this.setState({number2: this.state.number2 * 10 + number})
+                                            if ( this.state.number == 1 ) {
+                                                this.setState( {number1: this.state.number1 * 10 + number} )
+                                            } else {
+                                                this.setState( {number2: this.state.number2 * 10 + number} )
                                             }
                                         }}
                                     >
@@ -46,20 +61,22 @@ class App extends Component {
                     </div>
                     <div className="actions">
                         <button onClick={() => {
-                            this.setState({operand: '/', number: 2})
+                            this.setState( {operand: '/', number: 2} )
                         }}>&divide;</button>
 
                         <button onClick={() => {
-                            this.setState({operand: '*', number: 2})
+                            this.setState( {operand: '*', number: 2} )
                         }}>&times;</button>
 
                         <button onClick={() => {
-                            this.setState({operand: '-', number: 2})
-                        }}>-</button>
+                            this.setState( {operand: '-', number: 2} )
+                        }}>-
+                        </button>
 
                         <button onClick={() => {
-                            this.setState({operand: '+', number: 2})
-                        }}>+</button>
+                            this.setState( {operand: '+', number: 2} )
+                        }}>+
+                        </button>
                     </div>
                 </div>
             </div>
